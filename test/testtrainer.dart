fc5914a1cb474baf98f83f9ec05b6aff7ca8a01a
martin.haesler <martin.haesler@gmail.com>
2015-04-11 18:04:07 +0100

    Add test for currentposbackward.

--- test/testtrainer.dart
+++ test/testtrainer.dart
@@ -3,14 +3,25 @@ import 'package:unittest/unittest.dart';
 import 'package:workmantrainer/trainer.dart' show Trainer;
 
 void main() {
-  var tr = new Trainer();
+
   test('initialize class', () {
+    var tr = new Trainer();
     expect(tr.currentposition, 0);
   });
   test('set position forward', () {
+    var tr = new Trainer();
     tr.currentposforward();
     tr.currentposforward();
     tr.currentposforward();
     expect(tr.currentposition, 3);
+  });
+  test('set position backward', () {
+    var tr = new Trainer();
+    tr.currentposforward();
+    tr.currentposforward();
+    expect(tr.currentposition, 2);
+    tr.currentposbackward();
+    tr.currentposbackward();
+    expect(tr.currentposition, 0);
   });
 }
