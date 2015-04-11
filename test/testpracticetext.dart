87b22d41128beef906bb3ee8471cdf97e884e07e
martin.haesler <martin.haesler@gmail.com>
2015-04-11 11:33:07 +0100

    Add practicetext function

--- test/testpracticetext.dart
+++ test/testpracticetext.dart
@@ -1,9 +1,13 @@
 import 'package:unittest/unittest.dart';
 import 'package:unittest/html_config.dart';
 import 'dart:html';
-import 'package:workmantrainer/practicetext.dart';
+import 'package:workmantrainer/practicetext.dart' show practiceText;
 
 void main() {
-  
+  var target = document.querySelector('#targettext');
+  practiceText('Test Text 1');
+  test('set practice text', () {
+    expect(target.text, 'Test Text 1');
+  });
 }
 
