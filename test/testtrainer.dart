53f9e5d8f0114d4a66e78e16c653e87b70ed9880
martin.haesler <martin.haesler@gmail.com>
2015-04-11 19:09:37 +0100

    Calculate accuracy test.

--- test/testtrainer.dart
+++ test/testtrainer.dart
@@ -5,18 +5,19 @@ import 'package:workmantrainer/trainer.dart' show Trainer;
 void main() {
 
   test('initialize class', () {
-    var tr = new Trainer();
+    var tr = new Trainer('some text');
     expect(tr.currentposition, 0);
+    expect(tr.targettext, 'some text');
   });
   test('set position forward', () {
-    var tr = new Trainer();
+    var tr = new Trainer('');
     tr.currentposforward();
     tr.currentposforward();
     tr.currentposforward();
     expect(tr.currentposition, 3);
   });
   test('set position backward', () {
-    var tr = new Trainer();
+    var tr = new Trainer('');
     tr.currentposforward();
     tr.currentposforward();
     expect(tr.currentposition, 2);
@@ -24,4 +25,11 @@ void main() {
     tr.currentposbackward();
     expect(tr.currentposition, 0);
   });
+  test('calculate accuracy', () {
+    var tr = new Trainer('some text');
+    tr.errors = 2;
+    expect(tr.accuracy(), 78);
+    
+  });
+  
 }
