bdb941e6c8d06fe284c43bccbbffbdb3734f914b
martin.haesler <martin.haesler@gmail.com>
2015-04-11 17:43:03 +0100

    Delete obsolete test file.

--- test/testpracticetext.dart
+++ test/testpracticetext.dart
@@ -1,11 +1,16 @@
 import 'package:unittest/unittest.dart';
 import 'dart:html';
 import 'package:workmantrainer/practicetext.dart' show practiceText;
+import 'package:workmantrainer/trainer.dart' show Trainer;
 
 void main() {
   var target = document.querySelector('#targettext');
   practiceText('Test Text 1');
   test('set practice text', () {
     expect(target.text, 'Test Text 1');
+  });
+  var t1 = new Trainer();
+  test('initialize class', () {
+    expect(t1.currentposition, 0);
   });
 }
