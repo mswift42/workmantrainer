1db7e0e2c7ea575944e96dfeda55697c85738e6f
martin.haesler <martin.haesler@gmail.com>
2015-04-11 10:51:32 +0100

    Add Test folder

---
+++ test/testpracticetext.dart
@@ -0,0 +1,9 @@
+import 'package:unittest/unittest.dart';
+import 'package:unittest/html_config.dart';
+import 'dart:html';
+import 'package:workmantrainer/practicetext.dart';
+
+void main() {
+  
+}
+
