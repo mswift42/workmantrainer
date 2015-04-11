f6129ab26264ab96a482ab8ed1f766514f0f2c9b
martin.haesler <martin.haesler@gmail.com>
2015-04-11 14:10:21 +0100

    Add test for currentposition.

--- test/trainer_test.dart
+++ test/trainer_test.dart
@@ -1,4 +1,11 @@
 import 'package:unittest/unittest.dart';
-import 'package:workmantrainer/trainer.dart';
+import 'package:workmantrainer/trainer.dart' show Trainer;
+
+void main() {
+  var tr = new Trainer();
+  test('initialize class', () {
+    expect(tr.currentposition, 0)
+      });
+}
 
 
