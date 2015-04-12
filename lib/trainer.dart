00645e0d3496becba19ce63f1597eb81c2fe6f14
martin.haesler <martin.haesler@gmail.com>
2015-04-12 09:55:20 +0100

    Calculate accuracy only until current position.

--- lib/trainer.dart
+++ lib/trainer.dart
@@ -7,12 +7,19 @@ class Trainer{
   int _errors;
 
   Trainer(this.targettext);
+  
   int get currentposition => _currentposition;
+  void set currentposition(int n) => _currentposition = n;
+  
   int currentposforward() => _currentposition++;
+  
   int currentposbackward()=> _currentposition--;
+  
   set errors(int n) => _errors = n;
   
   int accuracy() {
-    return 100 - (_errors.toDouble() / targettext.length.toDouble() * 100).round();
+    var sofar = targettext.split('').getRange(0, _currentposition + 1);
+    return 100 - (_errors.toDouble() / sofar.length.toDouble() * 100).round();
+
   }
 }
