89b2b6b3f385102f08e2662930b125ddf93ba67e
martin.haesler <martin.haesler@gmail.com>
2015-04-13 17:13:44 +0100

    remove dangling closing brace.

--- lib/trainer.dart
+++ lib/trainer.dart
@@ -3,29 +3,27 @@ library trainer;
 import 'dart:html';
 import 'dart:convert';
 
-
-class Trainer{
+class Trainer {
   int _currentposition = 0;
   String targettext;
   int _errors;
 
   Trainer(this.targettext);
-  
+
   int get currentposition => _currentposition;
   set currentposition(int n) => _currentposition = n;
-  
+
   int currentposforward() => _currentposition++;
-  
-  int currentposbackward()=> _currentposition--;
+
+  int currentposbackward() => _currentposition--;
 
   String charAtPosition() => targettext.split('')[_currentposition];
-  
+
   set errors(int n) => _errors = n;
-  
+
   int accuracy() {
     var sofar = targettext.split('').getRange(0, _currentposition + 1);
     return 100 - (_errors.toDouble() / sofar.length.toDouble() * 100).round();
-
   }
   void handleKeyEvent(Event e) {
     KeyEvent k = new KeyEvent.wrap(e);
@@ -39,21 +37,15 @@ class Trainer{
         }
         break;
     }
-        
-        
+  }
 }
-
 void initTrainer() {
   var target = querySelector('#targettext').text;
   var tr = new Trainer(target);
   var input = querySelector('#tarea');
   input.onKeyUp.listen((e) {
-        while (tr.currentposition < target.length) {
-          
-    tr.handleKeyEvent(e);
-        }
-      });
+    while (tr.currentposition < target.length) {
+      tr.handleKeyEvent(e);
+    }
+  });
 }
-      
-
-
