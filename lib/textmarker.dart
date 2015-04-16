340ecba38ac799d3f4e10f4bf7521f6855681b8c
martin.haesler <martin.haesler@gmail.com>
2015-04-16 18:08:51 +0100

    Add initialization to textmarker class.

--- lib/textmarker.dart
+++ lib/textmarker.dart
@@ -1,2 +1,14 @@
 library textmarker;
 
+import 'dart:html';
+
+class TextMarker {
+  List<String> characters;
+  int startposition;
+  int endposition;
+  enum textState(CORRECT, INCORRECT, TOBETYPED);
+
+  TextMarker(this.characters,this.startposition, this.endposition,this.textState);
+}
+  
+
