6acc102b67cd55976fc75df073e513f91737df64
martin.haesler <martin.haesler@gmail.com>
2015-04-16 18:25:00 +0100

    Add test for Class initialisation.

--- test/testtextmarker.dart
+++ test/testtextmarker.dart
@@ -1,3 +1,12 @@
 import 'package:unittest/unittest.dart';
 import 'package:workmantrainer/textmarker.dart';
 
+void main() {
+  test('check initialisation of TextMarker Class works.', () {
+    var tm = new TextMarker('some text',0,2,"CORRECT");
+    expect(tm.targettext, 'some text');
+    expect(tm.startposition,0);
+    expect(tm.endposition,2);
+    expect(tm.textState, 'CORRECT');
+  });
+}
