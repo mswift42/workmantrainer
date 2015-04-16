1ff1581a5bb9e42bd868ddb1559ce4ebf75e028f
martin.haesler <martin.haesler@gmail.com>
2015-04-16 21:41:29 +0100

    Add textspan function.

--- lib/textmarker.dart
+++ lib/textmarker.dart
@@ -1,14 +1,21 @@
 library textmarker;
 
-import 'dart:html';
+import 'dart:html';     
 
 class TextMarker {
-  List<String> characters;
+  String targettext;
   int startposition;
   int endposition;
   String textState; // were characters from start to end position typed correctly.
 
-  TextMarker(this.characters,this.startposition, this.endposition,this.textState);
+  TextMarker(this.targettext,this.startposition, this.endposition,this.textState);
+
+  SpanElement textSpan(int start,end, String classname) {
+    var span = new SpanElement()
+      ..classes.add(classname)
+      ..text = targettext.substring(start,end);
+    return span;
+  }
 }
   
 
